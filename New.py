09d6b4a083f71fb18b4095281865e9c7fbae9d28
Bhargav <bhargav199805@gmail.com>
2024-08-13 15:26:48 +0530

    again

--- New.py
+++ New.py
@@ -1,1 +1,14 @@
-print("Hello Honey and Bhargav")
+# --------------------------- Python ----------------------------- #
+
+# ----- Python| output print() function ----- #
+"""python print() function used to print the messages to the screen or any other devices"""
+
+# Syntax : print(value(s), sep= ‘ ‘, end = ‘\n’, file=file, flush=flush)
+
+name = "Bhargav"
+age = 25
+
+print("Name:", name)
+print("Age:",age)
+
+print("hello my name is",name,"and my age is",age)
